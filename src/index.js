bafd22d740fbf61992f6879e9a2b5cd73173109f
quill_nichols <leplep00100@gmail.com>
2024-01-02 17:55:36 -0500

    pog

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const taskDescripton = document.getElementById('new-task-description').value;
+    const newTask = document.createElement('li');
+    newTask.taskContent = taskDescripton;
+    taskList.appendChild(newTask);
+    document.getElementById('new-task-description').value = '';
+  });
   // your code here
 });
